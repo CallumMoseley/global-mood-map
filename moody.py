1975dd0c02d5943fc2991b1a3aa5ada54ca466c2
RobbieZhuang <robbie.zhuang@gmail.com>
2017-09-10 01:46:49 -0400

    Added slider and fixed some stuff

--- moody.py
+++ moody.py
@@ -82,5 +82,5 @@ def socket(ws):
 
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
-server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+server = pywsgi.WSGIServer(('', 80), app, handler_class=WebSocketHandler)
 server.serve_forever()
